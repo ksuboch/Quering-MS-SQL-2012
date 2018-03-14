b6ca3a2a8673ed6e9afa60987da1bc7363b4338a
Suboch Kirill <3dvapper.inbox@gmail.com>
2018-03-14 20:56:50 +0400

    Lesson 01 - code and exercises

---
+++ Chapter 01/Code.sql
@@ -0,0 +1,37 @@
+﻿USE TSQL2012;
+
+--multiset
+SELECT
+  country
+FROM HR.Employees;
+
+--set
+SELECT DISTINCT
+  country
+FROM HR.Employees;
+
+--no sorting by default
+SELECT
+  empid
+ ,lastname
+FROM HR.Employees;
+
+--ordering by empid field
+SELECT
+  empid
+ ,lastname
+FROM HR.Employees
+ORDER BY empid;
+
+--non-relational ordering
+SELECT
+  empid
+ ,lastname
+FROM HR.Employees
+ORDER BY 1;
+
+--non-relational (no alias)
+ SELECT
+  empid,
+  firstname + ' ' + lastname --AS fullname
+ FROM HR.Employees;
---
+++ Chapter 01/Exercises.sql
@@ -0,0 +1,18 @@
+﻿USE TSQL2012;
+
+--non relatonal
+--1. no column alias
+--2. sorting use number of column
+--3. can return duplicates
+--4. result orders by something
+SELECT
+  custid
+ ,YEAR(orderdate)
+FROM Sales.Orders
+ORDER BY 1, 2;
+
+--relational
+SELECT DISTINCT
+  custid
+ ,YEAR(orderdate) AS orderyear
+FROM Sales.Orders;
