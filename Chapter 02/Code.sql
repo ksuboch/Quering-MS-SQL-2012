37bbcd7953bf611a7add8228735e7c0679bbb5a9
Kirill Suboch <3dvapper.inbox@gmail.com>
2018-03-15 14:57:32 +0400

    Chapter 02 - Lesson 02

--- Chapter 02/Code.sql
+++ Chapter 02/Code.sql
@@ -37,3 +37,87 @@ FROM HR.Employees AS e;
 SELECT
   10 AS col1
  ,'ABC' AS col2;
+
+--error
+SELECT
+  CAST('abc' AS INT);
+
+--NULL
+SELECT
+  TRY_CAST('abc' AS INT);
+
+--using style
+SELECT
+  CONVERT(DATE, '1/3/2012', 101);
+
+--using lang and regional parameters
+SELECT
+  PARSE('1/2/2012' AS DATE USING 'en-US');
+
+--string concatenation
+SELECT
+  empid
+ ,country
+ ,region
+ ,city
+ ,country + N',' + region + N',' + city AS location
+FROM HR.Employees;
+
+--using function coalesce
+SELECT
+  empid
+ ,country
+ ,region
+ ,city
+ ,country + COALESCE(N',' + region, N'') + N',' + city AS location
+FROM HR.Employees;
+
+--using function concat
+SELECT
+  empid
+ ,country
+ ,region
+ ,city
+ ,CONCAT(country, N',' + region, N',' + city) AS location
+FROM HR.Employees;
+
+--finding index
+SELECT
+  CHARINDEX('I', 'Itzic Ben-Gan');
+
+--editing strings
+SELECT
+  REPLACE('.1.2.3.', '.', '/');
+SELECT
+  REPLICATE('0', 10);
+SELECT
+  STUFF(',x,y,z', 1, 3, '-');
+
+--formatting string
+SELECT
+  FORMAT(1759, '0000000000');
+
+--CASE - simple form
+SELECT
+  p.productid
+ ,p.productname
+ ,p.unitprice
+ ,p.discontinued
+ ,CASE p.discontinued
+    WHEN 0 THEN 'No'
+    WHEN 1 THEN 'Yes'
+    ELSE 'Unknown'
+  END AS discounted_desc
+FROM Production.Products AS p;
+
+--CASE - searching form
+SELECT
+  productid
+ ,productname
+ ,unitprice
+ ,CASE
+    WHEN unitprice < 20.00 THEN 'Low'
+    WHEN unitprice < 40.00 THEN 'Medium'
+    WHEN unitprice >= 40.00 THEN 'High'
+  END
+FROM Production.Products;
--- Chapter 02/Exercises.sql
+++ Chapter 02/Exercises.sql
@@ -5,3 +5,25 @@ SELECT
  ,s.companyname
  ,s.phone AS [phone number]
 FROM Sales.Shippers AS s;
+
+SELECT
+  empid
+ ,firstname + N' ' + lastname AS fullname
+ ,YEAR(birthdate) AS birthyear
+FROM HR.Employees;
+
+SELECT
+  EOMONTH(SYSDATETIME()) AS end_of_current_month;
+
+SELECT
+  DATEFROMPARTS(YEAR(SYSDATETIME()), 12, 31) AS end_of_current_year;
+
+SELECT
+  productid
+ ,RIGHT(REPLICATE('0', 10) + CAST(productid AS VARCHAR(10)), 10) AS str_productid
+FROM Production.Products;
+
+SELECT
+  productid
+ ,FORMAT(productid, 'd10') AS str_productid
+FROM Production.Products;
