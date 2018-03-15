ed360c345816a1d543ed64afd5d112582ce4ef52
Kirill Suboch <3dvapper.inbox@gmail.com>
2018-03-15 16:35:49 +0400

    Chapter 03 - Lesson 01

---
+++ Chapter 03/Code.sql
@@ -0,0 +1,70 @@
+﻿USE TSQL2012;
+
+SELECT
+  empid
+ ,firstname
+ ,lastname
+ ,country
+ ,region
+ ,city
+FROM HR.Employees
+WHERE country = N'USA';
+
+--filtering
+SELECT
+  e.empid
+ ,e.firstname
+ ,e.lastname
+ ,e.country
+ ,e.region
+ ,e.city
+FROM HR.Employees AS e
+WHERE e.region = N'WA';
+
+--filtering (null values)
+SELECT
+  e.empid
+ ,e.firstname
+ ,e.lastname
+ ,e.country
+ ,e.region
+ ,e.city
+FROM HR.Employees AS e
+WHERE e.region <> N'WA'
+OR e.region IS NULL;
+
+--orders that was shipped
+SELECT
+  orderid
+ ,orderdate
+ ,empid
+FROM Sales.Orders
+WHERE shippeddate = @dt
+OR (shippeddate IS NULL
+AND @dt IS NULL);
+
+--filtering strings using template
+SELECT
+  empid
+ ,firstname
+ ,lastname
+FROM HR.Employees
+WHERE lastname LIKE N'D%';
+
+--filtering using data
+SELECT
+  orderid
+ ,empid
+ ,custid
+FROM Sales.Orders
+WHERE orderdate = '20070212';
+
+--don't use between; use >= and <
+SELECT
+  orderid
+ ,orderdate
+ ,empid
+ ,custid
+FROM Sales.Orders
+WHERE orderdate >= '20070201'
+AND orderdate < '20070301';
---
+++ Chapter 03/Exercises.sql
@@ -0,0 +1,37 @@
+﻿USE TSQL2012;
+
+--error filtering
+SELECT
+  orderid
+ ,orderdate
+ ,custid
+ ,empid
+FROM Sales.Orders
+WHERE shippeddate = NULL;
+
+--correct filtering
+SELECT
+  orderid
+ ,orderdate
+ ,custid
+ ,empid
+FROM Sales.Orders
+WHERE shippeddate IS NULL;
+
+--filtering using date column and BETWEEN
+SELECT
+  orderid
+ ,orderdate
+ ,custid
+ ,empid
+FROM Sales.Orders
+WHERE orderdate BETWEEN '20080211' AND '20080211 23:59:59.999';
+
+--correct filtering
+SELECT
+  orderid
+ ,orderdate
+ ,custid
+ ,empid
+FROM Sales.Orders
+WHERE orderdate >= '20080211' AND orderdate < '20080213';
