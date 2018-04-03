fe5c43300d73314244d9174bc324c1fb7658f2ad
Kirill Suboch <3dvapper.inbox@gmail.com>
2018-04-03 14:08:29 +0400

    Chapter 03 - Lesson 03 - Code

--- Chapter 03/Code.sql
+++ Chapter 03/Code.sql
@@ -34,14 +34,16 @@ WHERE e.region <> N'WA'
 OR e.region IS NULL;
 
 --orders that was shipped
+DECLARE @t AS DATETIME;
+
 SELECT
   orderid
  ,orderdate
  ,empid
 FROM Sales.Orders
-WHERE shippeddate = @dt
+WHERE shippeddate = @t
 OR (shippeddate IS NULL
-AND @dt IS NULL);
+AND @t IS NULL);
 
 --filtering strings using template
 SELECT
@@ -132,3 +134,102 @@ SELECT orderid, shippeddate
 FROM Sales.Orders
 WHERE custid = 20
 ORDER BY shippeddate;
+
+--limit selection
+SELECT TOP(3)
+   orderid
+  ,orderdate
+  ,custid
+  ,empid
+FROM Sales.Orders
+ORDER BY orderdate DESC;
+
+--total with percent
+SELECT TOP(1) PERCENT
+   orderid
+  ,orderdate
+  ,custid
+  ,empid
+FROM Sales.Orders
+ORDER BY orderdate DESC;
+
+--top with vars
+DECLARE @n AS BIGINT = 5;
+
+SELECT TOP (@n)
+   orderid
+  ,orderdate
+  ,custid
+  ,empid
+FROM Sales.Orders
+ORDER BY orderdate DESC;
+
+--three any rows
+SELECT TOP (3)
+   orderid
+  ,orderdate
+  ,custid
+  ,empid
+FROM Sales.Orders
+ORDER BY (SELECT NULL);
+
+--determin result
+SELECT TOP(3) WITH TIES
+   orderid
+  ,orderdate
+  ,custid
+  ,empid
+FROM Sales.Orders
+ORDER BY orderdate DESC;
+
+--using offset - fetch
+SELECT
+   orderid
+  ,orderdate
+  ,custid
+  ,empid
+FROM Sales.Orders
+ORDER BY orderdate DESC, orderid DESC
+OFFSET 50 ROWS FETCH NEXT 25 ROWS ONLY;
+
+--top analogue
+SELECT
+   orderid
+  ,orderdate
+  ,custid
+  ,empid
+FROM Sales.Orders
+ORDER BY orderdate DESC, orderid DESC
+OFFSET 0 ROWS FETCH FIRST 25 ROWS ONLY;
+
+--offset without fetch
+SELECT
+   orderid
+  ,orderdate
+  ,custid
+  ,empid
+FROM Sales.Orders
+ORDER BY orderdate DESC, orderid DESC
+OFFSET 50 ROWS;
+
+--select any rows
+SELECT
+   orderid
+  ,orderdate
+  ,custid
+  ,empid
+FROM Sales.Orders
+ORDER BY (SELECT NULL)
+OFFSET 0 ROWS FETCH FIRST 3 ROWS ONLY;
+
+--pager
+DECLARE @pagesize AS BIGINT = 25, @pagenum AS BIGINT = 3;
+
+SELECT
+   orderid
+  ,orderdate
+  ,custid
+  ,empid
+FROM Sales.Orders
+ORDER BY orderdate DESC, orderid DESC
+OFFSET (@pagenum - 1) * @pagesize ROWS FETCH NEXT @pagesize ROWS ONLY;
