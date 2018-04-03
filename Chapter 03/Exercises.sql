a2231c3e2c7aaac5c3d3a89a8e892e9216696a2b
Kirill Suboch <3dvapper.inbox@gmail.com>
2018-04-03 10:15:48 +0400

    Chapter 03 - Lesson 03 - Exercises

--- Chapter 03/Exercises.sql
+++ Chapter 03/Exercises.sql
@@ -35,3 +35,32 @@ SELECT
  ,empid
 FROM Sales.Orders
 WHERE orderdate >= '20080211' AND orderdate < '20080213';
+
+--getting client ordders by clientid
+SELECT
+   orderid
+  ,empid
+  ,shipperid
+  ,shippeddate
+FROM Sales.Orders
+WHERE custid = 77;
+
+--getting client ordders by clientid with ordering
+SELECT
+   orderid
+  ,empid
+  ,shipperid
+  ,shippeddate
+FROM Sales.Orders
+WHERE custid = 77
+ORDER BY shipperid;
+
+--determin ordering
+SELECT
+   orderid
+  ,empid
+  ,shipperid
+  ,shippeddate
+FROM Sales.Orders
+WHERE custid = 77
+ORDER BY shipperid, shippeddate DESC, orderid DESC;
