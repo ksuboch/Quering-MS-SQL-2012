e73e8316450daeba1672fcd69852a6d37a61212c
Kirill Suboch <3dvapper.inbox@gmail.com>
2018-04-04 12:04:35 +0400

    Chapter 04 - Lesson 02 - Exercises

--- Chapter 04/Exercises.sql
+++ Chapter 04/Exercises.sql
@@ -31,3 +31,67 @@ FROM Sales.Customers AS C
     LEFT OUTER JOIN Sales.Orders AS O
         ON C.custid = O.custid
 WHERE O.orderid IS NULL;
+
+--CTE
+SELECT
+     categoryid
+    ,MIN(unitprice) AS mn
+FROM Production.Products
+GROUP BY categoryid;
+
+WITH CatMin AS
+    (SELECT
+        categoryid
+        ,MIN(unitprice) AS mn
+    FROM Production.Products
+    GROUP BY categoryid)
+SELECT
+     P.categoryid
+    ,P.productid
+    ,P.productname
+    ,P.unitprice
+FROM Production.Products AS P
+    INNER JOIN CatMin AS M
+        ON P.categoryid = M.categoryid
+            AND P.unitprice = M.mn;
+
+--apply
+IF OBJECT_ID('Production.GetTopProducts', 'IF') IS NOT NULL
+DROP FUNCTION Production.GetTopProducts;
+GO
+
+CREATE FUNCTION Production.GetTopProducts(@supplierid AS INT, @n AS BIGINT)
+RETURNS TABLE
+AS
+    RETURN
+    SELECT
+        productid
+        ,productname
+        ,unitprice
+    FROM Production.Products
+    WHERE supplierid = @supplierid
+    ORDER BY unitprice, productid
+    OFFSET 0 ROWS FETCH FIRST @n ROWS ONLY;
+GO
+
+SELECT * FROM Production.GetTopProducts(1, 2) AS P;
+
+SELECT
+     S.supplierid
+    ,S.companyname AS supplier
+    ,A.*
+FROM Production.Suppliers AS S
+    CROSS APPLY Production.GetTopProducts(S.supplierid, 2) AS A
+WHERE S.country = N'Japan';
+
+SELECT
+     S.supplierid
+    ,S.companyname AS supplier
+    ,A.*
+FROM Production.Suppliers AS S
+    OUTER APPLY Production.GetTopProducts(S.supplierid, 2) AS A
+WHERE S.country = N'Japan';
+
+IF OBJECT_ID('Production.GetTopProducts', 'IF') IS NOT NULL
+DROP FUNCTION Production.GetTopProducts;
+GO
